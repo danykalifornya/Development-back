2a6dbbe5e6f7ff2478312a12d04f04c701a9803a
danykalifornya <danymtz1111@gmail.com>
2017-12-31 12:54:04 -0600

    tooltips added

--- common/models/lesson.js
+++ common/models/lesson.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function(Lesson) {
-
+  	/*Lesson.on('dataSourceAttached', function(obj){
+	    var find = MyModel.find;
+	    MyModel.find = function(filter, cb) {
+	      return find.apply(this, arguments);
+	    };
+  	});*/
+  	Lesson.afterRemote( "find", function( ctx, remoteMethodOutput, next) {
+	    console.log(remoteMethodOutput);
+	    next();
+	});
 };
